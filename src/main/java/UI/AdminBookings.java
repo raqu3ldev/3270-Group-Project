e4541e69adb08b783917c819fa8785df67f3c70f
nnamb <namdinantambu1@gmail.com>
2025-12-08 13:15:10 -0500

    made updates

--- src/main/java/UI/AdminBookings.java
+++ src/main/java/UI/AdminBookings.java
@@ -1,32 +1,25 @@
 package UI;
 
-import Model.User;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class AdminBookings {
-    private Stage stage;
-    private User user;
 
-    public AdminBookings(Stage stage, User user) {
+    private Stage stage;
+
+    public AdminBookings(Stage stage) {
         this.stage = stage;
-        this.user = user;
     }
 
     public void show() throws IOException {
         stage.setTitle("All Bookings");
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/UI/adminBookings.fxml"));
-        Scene scene = new Scene(loader.load(), 900, 500);
-
-        AdminBookingsController controller = loader.getController();
-        controller.setUser(user);
-        controller.setStage(stage);
+        Scene scene = new Scene(loader.load(), 769, 414);
 
         stage.setScene(scene);
         stage.show();
     }
 }
-
--- src/main/java/UI/AdminManageFlights.java
+++ src/main/java/UI/AdminManageFlights.java
@@ -1,6 +1,5 @@
 package UI;
 
-import Model.User;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,22 +8,16 @@ import java.io.IOException;
 public class AdminManageFlights {
 
     private Stage stage;
-    private User user;
 
-    public AdminManageFlights(Stage stage, User user) {
+    public AdminManageFlights(Stage stage) {
         this.stage = stage;
-        this.user = user;
     }
 
     public void show() throws IOException {
         stage.setTitle("Manage Flights");
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/UI/adminManageFlights.fxml"));
-        Scene scene = new Scene(loader.load(), 900, 600);
-
-        AdminManageFlightsController controller = loader.getController();
-        controller.setUser(user);
-        controller.setStage(stage);
+        Scene scene = new Scene(loader.load(), 769, 414);
 
         stage.setScene(scene);
         stage.show();
